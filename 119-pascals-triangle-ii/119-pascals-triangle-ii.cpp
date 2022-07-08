8e89d83f89ee8b46c92747f1ea740c3a12807f93
Masquerade <83944275+RishiRaj29@users.noreply.github.com>
2022-07-08 10:32:01 +0530

    Time: 3 ms (44.81%), Space: 6.5 MB (49.91%) - LeetHub

--- 119-pascals-triangle-ii/119-pascals-triangle-ii.cpp
+++ 119-pascals-triangle-ii/119-pascals-triangle-ii.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
     vector<int> getRow(int row) {
-        vector<vector<int>> pascal;
-        int i,j,ele;
-        for(i=0;i<=row;i++)
+        if(row==0)
+            return {1};
+        if(row==1)
+            return {1,1};
+        if(row==2)
+            return {1,2,1};
+        vector<int> ans={1,2,1};
+        int i,j;
+        for(i=3;i<=row;i++)
         {
-            vector<int>temp;
-            for(j=0;j<=i;j++)
+            vector<int> temp;
+            temp.push_back(1);
+            for(j=1;j<i;j++)
             {
-                if(j==0)
-                    temp.push_back(1);
-                else if(i==j)
-                    temp.push_back(1);
-                else
-                    temp.push_back(pascal[i-1][j-1]+pascal[i-1][j]);
+                temp.push_back(ans[j]+ans[j-1]);
             }
-            pascal.push_back(temp);
+            temp.push_back(1);
+            ans=temp;
         }
-        return pascal[row];
+        return ans;
     }
 };
